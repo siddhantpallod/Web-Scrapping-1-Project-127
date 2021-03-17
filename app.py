95144e4debf35fa14093ccf7a360d3a5319d95b6
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2021-03-17 09:55:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup as bs
 import requests
-from selenium import webdriver
 import pandas as pd
 
 url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
@@ -25,14 +24,16 @@ Star_Names = []
 Distance =[]
 Mass = []
 Radius =[]
+Luminosity = []
 
 for i in range(1, len(tempList)):
     Star_Names.append(tempList[i][1])
     Distance.append(tempList[i][3])
     Mass.append(tempList[i][5])
     Radius.append(tempList[i][6])
+    Luminosity.append(tempList[i][7])
   
 
 
-df = pd.DataFrame(list(zip(Star_Names,Distance,Mass,Radius)),columns=['Star_name','Distance','Mass','Radius'])
+df = pd.DataFrame(list(zip(Star_Names,Distance,Mass,Radius,Luminosity)),columns=['Star_name','Distance','Mass','Radius', 'Luminosity'])
 df.to_csv('final.csv')
